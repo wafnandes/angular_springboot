966fea0fdc1458b0d9a41a8d2113722ce7bbf8a4
wafnandes <cmp.warley@gmail.com>
2021-04-14 16:57:55 -0300

    Serviço de tarefas implementado

--- Front/tarefas/src/app/services/tarefas.service.ts
+++ Front/tarefas/src/app/services/tarefas.service.ts
@@ -3,6 +3,8 @@ import { Injectable } from '@angular/core';
 import { environment } from 'src/environments/environment';
 import { Tarefas } from '../models/tarefas';
 import { Observable } from 'rxjs';
+import { MatSnackBar } from '@angular/material/snack-bar';
+import { validateHorizontalPosition } from '@angular/cdk/overlay';
 
 @Injectable({
   providedIn: 'root'
@@ -11,9 +13,23 @@ export class TarefasService {
 
   baseUrl = environment.baseUrl;
 
-  constructor(private http: HttpClient) { }
+  constructor(private http: HttpClient, private snack: MatSnackBar) { }
 
   findAll(): Observable<Tarefas[]> {
     return this.http.get<Tarefas[]>(this.baseUrl);
+  }
+
+  delete(id: any): Observable<void>{
+    const url = `${this.baseUrl}/${id}`
+    return this.http.delete<void>(url);
+  }
+
+  message(msg: String): void{
+    this.snack.open(`${msg}`, 'OK', {
+        horizontalPosition: 'end',
+        verticalPosition: 'top',
+        duration: 4000
+      }
+    )
   }
 }
