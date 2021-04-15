b7f8e54e04024d4b1423043d6d36bc2db06dcfc9
wafnandes <cmp.warley@gmail.com>
2021-04-14 21:51:00 -0300

    Rotas configuradas.

--- Front/tarefas/src/app/app-routing.module.ts
+++ Front/tarefas/src/app/app-routing.module.ts
@@ -1,7 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ReadAllComponent } from './components';
+import { FinalizadosComponent } from './components/finalizados/finalizados.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path:'',
+    component: ReadAllComponent
+  },
+  {
+    path:'finalizados',
+    component: FinalizadosComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
