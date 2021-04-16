3c79fcfb24853d232e363867fe1ec2b80d80a94a
wafnandes <cmp.warley@gmail.com>
2021-04-16 16:06:07 -0300

    Backend implementado.

--- Backend/tarefas/src/main/java/com/warley/tarefas/services/DBService.java
+++ Backend/tarefas/src/main/java/com/warley/tarefas/services/DBService.java
@@ -26,14 +26,8 @@ public class DBService {
 
 		Tarefas t1 = new Tarefas(null, "Estudar", "Estudar SpringBoot 2 e Angular 11",
 				sdf.parse("25/03/2022"), false);
-		Tarefas t2 = new Tarefas(null, "Ler", "Ler documentação Angular",
-				sdf.parse("04/04/2021"), true);
-		Tarefas t3 = new Tarefas(null, "Ler", "Ler documentação SpringBoot",
-				sdf.parse("05/04/2021"), true);
-		Tarefas t4 = new Tarefas(null, "Meditar", "Meditar durante 30 minutos pela manhã",
-				sdf.parse("13/04/2021"), false);
 
-		tarefasRepository.saveAll(Arrays.asList(t1, t2, t3, t4));
+		tarefasRepository.saveAll(Arrays.asList(t1));
 	}
 
 }
